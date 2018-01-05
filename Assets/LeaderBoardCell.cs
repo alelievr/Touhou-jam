50291ec42f1aec347835ff4df72c24c06d35d0c3
alelievr <alelievr@student.42.fr>
2018-01-05 19:46:22 +0100

    begin leaderboard

---
+++ Assets/LeaderBoardCell.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LeaderBoardCell : MonoBehaviour
+{
+	public Image	fillImage;
+	public Text		pointText;
+	public Text		nameText;
+
+	public void UpdateProperties(int points, int maxPoints, string name)
+	{
+		fillImage.fillAmount = (float)points / (float)maxPoints;
+
+		pointText.text = points + "/" + maxPoints;
+
+		nameText.text = name;
+	}
+}
