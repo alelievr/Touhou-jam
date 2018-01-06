e587b651dcff6c37d504f47afed9547458adcb53
Babacar CISS <bciss@e1r11p3.42.fr>
2018-01-06 22:14:37 +0100

    pfff tg

--- Assets/Scripts/seikuken.cs
+++ Assets/Scripts/seikuken.cs
@@ -5,20 +5,29 @@ using UnityEngine;
 public class seikuken : MonoBehaviour {
 	public	List<ParticleSystem.Particle> inside;
 	public	GameObject	Dodger;
-	public	GameObject	Limits;
+	// public	GameObject	Limits;
+	public	GameObject	Player;
+
+	[Space]
+	public float		dodgePower;
 
 	private	Rigidbody	rb;
 	private	bool		inLimits;
+	private bool		busy;
+	private	float		starty;
+	private Vector3		dodgerpos;
 	// Use this for initialization
 	void Start () {
 		rb = Dodger.GetComponent<Rigidbody>();
 		inLimits = true;
+		busy = false;
+		starty = transform.position.y;
 	}
 	
-	void FixedUpdate()
-	{
+	void FixedUpdate() {
 		Refresh();
-		Dodge();
+		if (!Dodge())
+			Move();
 	}
 
 	// Update is called once per frame
@@ -26,26 +35,54 @@ public class seikuken : MonoBehaviour {
 		
 	}
 
-	void Refresh()
-	{
+	void Refresh() {
 		rb.velocity = Vector3.zero;
+		busy = false;
 	}
 
-	void Dodge()
-	{
+	bool Move() {	
+		return true;
+	}
+
+	bool Dodge() {
+		Vector3 dir = Vector3.zero;
 		if (inside != null) {
-			Vector3 dir = new Vector3(0,0);
 			for (int i = 0; i < inside.Count; i++) {
-				// Debug.Log(inside[i].position);
-				dir += -(inside[i].position - transform.position).normalized * (1 - ((inside[i].position - transform.position).magnitude / 1.5f)); // prendre radius
+				// Debug.Log((inside[i].position - transform.position).magnitude);
+				Vector3 delta = inside[i].position - transform.position;
+				Vector3 direction = -delta.normalized;
+				float directionForce = dodgePower / delta.magnitude;
+				Debug.DrawRay(transform.position, direction * directionForce, Color.cyan);
+				dir += direction * directionForce; // prendre radius
 			}
-			if (!inLimits) {
-				dir += (Limits.transform.position - transform.position);
-				// Debug.Log(Limits.transform.position - transform.position);
-			}
-			Debug.DrawRay(transform.position,dir, Color.green);
-			rb.AddForce(dir * 100);
 		}
+		// if (!inLimits) {
+		// 	dir += (Limits.transform.position - transform.position);
+		// 	// Debug.Log(Limits.transform.position - transform.position);
+		// }
+		
+		dir = new Vector3(dir.x * 3f, dir.y, dir.z);
+		
+		// reste en face du joueur et ne monte pas trop haut
+		Vector3 targetx = new Vector3(Player.transform.position.x, starty, 0);
+		Vector3 deltaX = targetx - transform.position;
+		Vector3 directionX = deltaX.normalized * Mathf.Clamp(Mathf.Exp(deltaX.magnitude / 3), 0, 1000);
+		Debug.DrawRay(transform.position, directionX, Color.red);
+		dir += directionX;
+
+		Vector3 targety = new Vector3(0, starty, 0);
+		dir += (targety - transform.position) / 1.5f;
+		Debug.DrawRay(transform.position,dir, Color.green);
+		
+		if (dir != Vector3.zero){
+			rb.AddForce(dir * 100);
+			return true;
+		}
+		return false;
+	}
+
+	Vector3 getDodgerPos() {
+		return (new Vector3(transform.position.x, transform.position.y + 0.3f, 0));
 	}
 
 	void OnTriggerEnter(Collider other)
